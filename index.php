04798dbf6b32a7cf5294ae6805cdc8ae2ee1780d
Malikabhc <malika.bouhcine@gmail.com>
2018-08-20 11:09:10 +0200

    exercice2-6

--- index.php
+++ index.php
@@ -8,12 +8,14 @@
     <!-- exo02-6/?lastname=Nemare&firstname=Jean -->
       <p>
       <?php
-        if (isset($_GET['age'])) {
-        echo $_GET['age'] . ' ';
-      } else {
-        echo 'Veuillez saisir votre âge';
-      }
-      ?>
+        //Création d'une condition si le paramètre âge existe
+        if(!empty($_GET['lastname']) && !empty($_GET['firstname']) && !empty($_GET['age'])){
+         echo $_GET['lastname'] . ' ' . $_GET['firstname'] . ' ' . $_GET['age'];
+        } else {
+        //Autre condition si le paramètre age n'est pas présent
+          echo 'Il manque l\'âge';
+        }
+       ?>
       </p>
   </body>
 </html>
